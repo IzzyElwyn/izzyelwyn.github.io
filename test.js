0bb669b2fb6692615b6d2d1dc123a8e1c02d72c8
Izzy Elwyn <izzy.elwyn@gmail.com>
2018-10-23 00:28:40 -0400

    uuuugh

--- test.js
+++ test.js
@@ -27,19 +27,4 @@ function showSlides(n) {
 }
 
 var prevButton = document.getElementById('prev');
-prevButton.addEventListener('click', function({
-  var i = -1;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-})
-//prevButton.addEventListener('click', plusSlides(-1));
+prevButton.addEventListener('click', plusSlides(-1));
