df6bd02e9ddde96004c2767a8224295166ae5290
Izzy Elwyn <izzy.elwyn@gmail.com>
2019-01-25 21:15:34 -0500

    headshot shenanigans

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ let btn = document.getElementById('jackalope');
 
 let span = document.getElementsByClassName("close")[0];
 
+let img = document.getElementById('it-me');
+
 btn.onclick = function() {
   modal.style.display = "block";
+  img.style.zIndex = "0";
+
 }
 
 span.onclick = function() {
   modal.style.display = "none";
+  img.style.zIndex = "1";
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    img.style.zIndex = "1";
   }
 }
