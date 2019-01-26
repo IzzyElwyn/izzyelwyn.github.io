92de6a4203b670641f5065b12f9e2e6b37df92ba
Izzy Elwyn <izzy.elwyn@gmail.com>
2019-01-25 19:51:13 -0500

    adding a bit

---
+++ script.js
@@ -0,0 +1,19 @@
+let modal = document.getElementById('secret-modal');
+
+let btn = document.getElementById('jackalope');
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
