77ac5c808577b202ab147b2298ccddc4138f1720
Izzy Elwyn <izzy.elwyn@gmail.com>
2019-01-27 05:10:32 -0500

    updating for style consistency

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ let modal = document.getElementById('secret-modal');
 
 let btn = document.getElementById('jackalope');
 
+let logo =
+document.getElementsByClassName("logo")[0];
+
+let hvlogo =
+document.getElementsByClassName("hover-logo")[0];
+
 let span = document.getElementsByClassName("close")[0];
 
 let img = document.getElementById('it-me');
+
+btn.onmouseover = function() {
+  logo.style.display = "none";
+  hvlogo.style.display = "block";
+}
+
+btn.onmouseleave = function() {
+  logo.style.display = "block";
+  hvlogo.style.display = "none";
+}
 
 btn.onclick = function() {
   modal.style.display = "block";
